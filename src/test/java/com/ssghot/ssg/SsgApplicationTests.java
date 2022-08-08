737a9c529868028e48fea0a0b58ac1b0fa713fbd
yjg1 <zxcvc9999@gmail.com>
2022-08-08 12:15:13 +0900

    setting

---
+++ src/test/java/com/ssghot/ssg/SsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssghot.ssg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
