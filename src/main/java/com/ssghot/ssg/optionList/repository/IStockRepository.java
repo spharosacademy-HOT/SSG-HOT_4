2a2aad4c15ae39979e2a4391a475695a03747ec0
yjg1 <zxcvc9999@gmail.com>
2022-08-11 16:50:35 +0900

    optionlist2

--- src/main/java/com/ssghot/ssg/optionList/repository/IStockRepository.java
+++ src/main/java/com/ssghot/ssg/optionList/repository/IStockRepository.java
@@ -3,5 +3,5 @@ package com.ssghot.ssg.optionList.repository;
 import com.ssghot.ssg.optionList.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IStockRepository extends JpaRepository<Stock, Integer> {
+public interface IStockRepository extends JpaRepository<Stock, Long> {
 }
