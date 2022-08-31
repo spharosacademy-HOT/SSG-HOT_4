5b1a2d9c10ecfd947497dffdd832cd913a5b798e
hoho <jhj20071@naver.com>
2022-08-31 13:21:29 +0900

    [#19] feat: option api 변경 연결

--- ssg-hot/src/store/apis/option.js
+++ ssg-hot/src/store/apis/option.js
@@ -1,13 +1,23 @@
-import { basicApiClient } from "./apiClient";
+import { apiClient, basicApiClient } from "./apiClient";
 
 //1 옵션으로 2조회
-export const getOption1 = async (id) => {
-  const res = await basicApiClient.get(`/option1/option2/${id}`);
+export const getOption1 = async (id, productId) => {
+  const res = await basicApiClient.get(
+    `/option1/option2/simple/${id}/${productId}`
+  );
   return res;
 };
 
 ///2옵션으로 1조회?
-export const getOption2 = async (id) => {
-  const res = await basicApiClient.get(`/option2/option1/${id}`);
+export const getOption2 = async (id, productId) => {
+  const res = await basicApiClient.get(
+    `/option2/option1/simple/${id}/${productId}`
+  );
   return res;
+};
+
+//옵션 변경
+export const putOption = async (optionData) => {
+  const res = await apiClient.put(`/carts/stock/v2`, optionData);
+  return res.data;
 };
