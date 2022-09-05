51d45a043beeb6f05a9a507e528fe6804e6a2ab0
Park su ah <tndk4997@naver.com>
2022-09-05 17:57:29 +0900

    [#19] style: qna css 수정

--- ssg-hot/src/pages/product/ProductQnaItem.jsx
+++ ssg-hot/src/pages/product/ProductQnaItem.jsx
@@ -1,29 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { useState } from "react";
 
 export default function ProductQnaItem({ data }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen=()=>{
+    setOpen(!open)
+  }
+  console.log('asdfasdfasdf')
+  console.log(data)
+
+  
   return (
-    <div>
+    <div className="qna-box">
       <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
+        // style={{
+        //   display: "flex",
+        //   justifyContent: "space-between",
+        //   alignItems: "center",
+        // }}
+        
       >
-        <span>답변대기</span>
-        <span>{data.createdDate}</span>
-        <span>{data.email}</span>
+        <span className="waiting-answer">답변대기 &nbsp;</span>
+        <span>{data.createdDate} &nbsp;</span>
+        <span>{data.email} </span>
       </div>
       <div>
         <span className="blind">문의내용</span>
-        <button type="button">
-          <span className="blind">
-            <span className="sr_off">문의내용 펼치기</span>
-            <span className="sr_on">문의내용 접기</span>
-          </span>
-          <em className="mndtl_qna_tit">{data.title}</em>
-          <p className="mndtl_qna_desc">{data.contents}</p>
-        </button>
+        <div className="mndtl_qna_tit">
+          <em>{data.title}</em>
+          <FontAwesomeIcon icon={faCaretDown} onClick={handleOpen}/>
+        </div>
+        <p className={open?"mndtl_qna_desc_clicked":"mndtl_qna_desc"}>{data.contents}</p>
+
       </div>
     </div>
   );
