2f668e6fd589632774b194a05f53022af3056249
Park su ah <tndk4997@naver.com>
2022-09-05 14:21:56 +0900

    [#19] feat: 구매하기 연동

--- ssg-hot/src/pages/product/ProductList.jsx
+++ ssg-hot/src/pages/product/ProductList.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { purchaseState } from '../../store/atom/purchaseState';
 import ProductCardList from './ProductCardList';
 import ProductCarosel from './ProductCarosel';
 import ProductNav from './ProductNav';
 
 function ProductList() {
+    const[purchaseList,setPurchaseList] = useRecoilState(purchaseState)
+
+    useEffect(()=>{
+        setPurchaseList([])
+    },[])
     return ( 
         <>
             <ProductNav />
